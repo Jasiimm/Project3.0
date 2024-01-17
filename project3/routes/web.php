3f8d3cb4856b811f93c6fa7349d5e218f83b6549
jasim <jasimagou@gmail.com>
2024-01-17 09:35:16 +0100

    Index

--- project3/routes/web.php
+++ project3/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/index', function () {return view('index');});
 
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/home', function () {return view('home');});
+
+Route::get('/pizzas', function () {return view('pizzas');});
+
+Route::get('/menu', function () { return view('menu');});
+
