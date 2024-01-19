7e67f9b776a0de70f0d8455734703c9d9d5ace8f
PS228954 <PS228954@edu.summacollege.nl>
2024-01-19 12:13:21 +0100

    nhbh

--- project3/resources/views/index.blade.php
+++ project3/resources/views/index.blade.php
@@ -1,13 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-    <div class="relative">
-    <!--Begin card -->
-     
-        <div class="w-60 h-96 bg-gray-800 p-3 flex flex-col gap-1 rounded-br-3xl mx-4">
+    <div class="flex justify-center items-center h-screen">
+        <div class="w-60 h-96 bg-gray-800 p-3 flex flex-col gap-1 rounded-br-3xl">
             <img src="{{ asset('images/chicken.png') }}" class="mx-3 h-32 object-cover" alt="Description">
-            <div class="flex flex-col gap-8 mt-4"> <!-- Add mt-4 for top margin -->
+            <div class="flex flex-col gap-8 mt-4">
                 <div class="flex flex-row justify-between">
                     <div class="flex flex-col">
                         <span class="text-xl text-gray-50 font-bold">Pizza Chicken</span>
@@ -18,9 +15,13 @@
                 <button class="hover:bg-sky-700 text-gray-50 bg-sky-800 py-2 rounded-br-xl">Add to cart</button>
             </div>
         </div>
+
+        <!-- Repeat the above card structure for other items -->
+
+        <!-- Card 2 -->
         <div class="w-60 h-96 bg-gray-800 p-3 flex flex-col gap-1 rounded-br-3xl mx-4">
             <img src="{{ asset('images/chicken.png') }}" class="mx-3 h-32 object-cover" alt="Description">
-            <div class="flex flex-col gap-8 mt-4"> <!-- Add mt-4 for top margin -->
+            <div class="flex flex-col gap-8 mt-4">
                 <div class="flex flex-row justify-between">
                     <div class="flex flex-col">
                         <span class="text-xl text-gray-50 font-bold">Pizza Chicken</span>
@@ -31,9 +32,11 @@
                 <button class="hover:bg-sky-700 text-gray-50 bg-sky-800 py-2 rounded-br-xl">Add to cart</button>
             </div>
         </div>
+
+        <!-- Card 3 -->
         <div class="w-60 h-96 bg-gray-800 p-3 flex flex-col gap-1 rounded-br-3xl mx-4">
             <img src="{{ asset('images/chicken.png') }}" class="mx-3 h-32 object-cover" alt="Description">
-            <div class="flex flex-col gap-8 mt-4"> <!-- Add mt-4 for top margin -->
+            <div class="flex flex-col gap-8 mt-4">
                 <div class="flex flex-row justify-between">
                     <div class="flex flex-col">
                         <span class="text-xl text-gray-50 font-bold">Pizza Chicken</span>
@@ -44,4 +47,6 @@
                 <button class="hover:bg-sky-700 text-gray-50 bg-sky-800 py-2 rounded-br-xl">Add to cart</button>
             </div>
         </div>
+    </div>
 @endsection
+
